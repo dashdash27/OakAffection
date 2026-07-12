bbd346685f81c391f493db2460a85b1c09045076
Daria Yatsenko <dasshh27@gmail.com>
2026-07-12 19:44:02 +0300

    feat(admin): add filter bar for orders

--- app/admin/orders_routes.py
+++ app/admin/orders_routes.py
@@ -1,14 +1,29 @@
 from app.admin.routes import admin_bp 
-from app.models import Order
+from app.models import Order, OrderStatus
 
 
 from flask import render_template, request, redirect, url_for, flash
 
 
-@admin_bp.route('/orders/home', methods=['GET', 'POST'])
-def orders_home():
-    orders = Order.query.all()
-    return render_template('admin/orders/home.html', orders=orders)
+@admin_bp.route('/orders', methods=['GET'])
+def orders():
+    status_filter = request.args.get('status')
+
+    query = Order.query.order_by(Order.created_at.desc())
+
+    if status_filter == 'active':
+        query = query.filter(Order.status != OrderStatus.CANCELLED)
+        
+    elif status_filter:
+        try:
+            enum_status = OrderStatus(status_filter)
+            query = query.filter(Order.status == enum_status)
+        except ValueError:
+            status_filter = None
+
+    orders = query.all()
+
+    return render_template('admin/orders/home.html', orders=orders, current_status=status_filter)
 
 @admin_bp.route('/orders/<int:order_id>', methods=['GET', 'POST'])
 def order_detail(order_id):
