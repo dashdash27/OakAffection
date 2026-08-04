2b3e549e9366901438f3025d72c29303670f5186
Daria Yatsenko <dasshh27@gmail.com>
2026-08-04 12:05:14 +0300

    refactor(logger): modernize logging service implementation

--- app/logger.py
+++ app/logger.py
@@ -18,7 +18,14 @@ def setup_logger():
     log.setLevel(logging.ERROR)
 
     logger = logging.getLogger('myapp')
-    logger.setLevel(logging.DEBUG)
+
+    is_debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ('true', '1')
+
+    if is_debug_mode:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
     logger.propagate = False  # предотвращаем дублирование
 
     formatter = logging.Formatter('%(asctime)s %(levelname)-8s [%(request_id)s] %(message)s')
@@ -27,15 +34,28 @@ def setup_logger():
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    file_handler = RotatingFileHandler(os.path.join(log_dir, 'app.log'), maxBytes=10*1024*1024, backupCount=5, encoding='utf-8')
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.addFilter(RequestIdFilter())
-
     # Добавляем обработчики только если их еще нет
     if logger.hasHandlers():
         logger.handlers.clear()
+
+    # --- ОБРАБОТЧИК 1: ЗАПИСЬ В ФАЙЛ ---
+    file_handler = RotatingFileHandler(
+        os.path.join(log_dir, 'app.log'), 
+        maxBytes=10*1024*1024, 
+        backupCount=5, 
+        encoding='utf-8'
+    )
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.addFilter(RequestIdFilter())
     logger.addHandler(file_handler)
+
+    # --- ОБРАБОТЧИК 2: ВЫВОД В КОНСОЛЬ ---
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.addFilter(RequestIdFilter())
+    logger.addHandler(console_handler)
 
     return logger
 
