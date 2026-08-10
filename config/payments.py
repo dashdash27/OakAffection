742dffcaafe4d1eebea2f057ef2747669128eade
Daria Yatsenko <dasshh27@gmail.com>
2026-08-10 18:07:28 +0300

    refactor(payment): extract order prefix to .env

--- config/payments.py
+++ config/payments.py
@@ -11,5 +11,5 @@ class PaymentsConfig:
         "NOTIFICATION_URL": f"{_base_url}/payments/api/ozon-pay-webhook",
         "SUCCESS_URL": f"{_base_url}/checkout/success",
         "FAILURE_URL": f"{_base_url}/checkout/failure",
-        "ORDER_PREFIX": "LOCAL2-"
+        "ORDER_PREFIX": os.getenv("OZON_PAY_ORDER_PREFIX")
     }
